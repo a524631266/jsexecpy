8c73ef75237b00f2b00e5ae6f3ac0cc1f3bbe408
zhangll <zhangll@homewell.com.cn>
2018-09-30 16:27:52 +0800

    添加执行ipynb文件，主要思路为使用jupyter convert命令转换文件，并把文件保存到系统的tmp目录中，成功完成之后正常处理python文件输出结果

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let {runpath,runpytext} = require("./runpython")
+let {run_ipynb_code} = require("./runipynb")
+
+
+module.exports = {
+    runpath,runpytext,run_ipynb_code
+}
--- test/test.js
+++ test/test.js
@@ -1,4 +1,9 @@
-let pyexec = require("../index");
-// pyexec.runpath("./test/test.py")
+// let pyexec = require("../index");
+// // pyexec.runpath("./test/test.py")
 
-pyexec.runpytext("import os;import time;time.sleep(1);print('you are my love');time.sleep(5);a = 2;a+=1;print(a)")
+// pyexec.runpytext("import os;import time;time.sleep(1);print('you are my love');time.sleep(5);a = 2;a+=1;print(a)")
+let {run_ipynb_code} = require("../index")
+
+run_ipynb_code("I:/家和工作记录/jupytertest/test.ipynb",function(data){
+    console.log(data);
+})
